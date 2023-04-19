9b2a6626075f7ea4962dbf445a3aeddd5670fcf9
harlamenkoel <harlamenkoel@gmail.com>
2023-04-20 00:58:54 +0700

    Выполнил

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -25,5 +25,9 @@ public class UserServiceImp implements UserService {
    public List<User> listUsers() {
       return userDao.listUsers();
    }
-
+   @Transactional
+   @Override
+   public User getUserCar(String model,int series){
+      return userDao.getUserCar(model,series);
+   }
 }
